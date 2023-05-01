2096d91ccdbb7fefc0aa7a71f8dd2958509f2b6c
0JUU <1754185@hanmail.net>
2023-05-01 18:57:38 +0900

    회원 웹 기능(홈, 등록, 조회)

--- src/main/java/api/apispring/controller/MemberController.java
+++ src/main/java/api/apispring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package api.apispring.controller;
 
+import api.apispring.domain.Member;
 import api.apispring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -38,4 +44,27 @@ public class MemberController {
      *     ==> return new DbMemberRepository();로 변경 가능(얘만 손대면 끝남)
      * }
     * */
+
+    @GetMapping("/members/new")  // get은 조회할 때 사용
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")  // post는 데이터 전달할 때 사용
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+
 }
